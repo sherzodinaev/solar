bcb80eb8b8b00a404068f9caeb7df2c8cd792fe8
Sherzod Odinaev <sherzod.odinaev@accelered.com>
2017-11-23 20:15:19 -0500

    task 001

---
+++ tasks/php/task001.php
@@ -0,0 +1,17 @@
+<?php
+/*
+
+! Learn string concatenation, or interpolation, or string formatting.
+
+Create 3 variables
+
+$age = 33;
+$name = "John";
+$languages = array('English', 'Uzbek', 'Russian');
+
+
+and create a function that produces below output
+
+echo get_task001($age, $name, $languages);
+> My name is John, I am 33 years old, and I speak following languages: English, Uzbek, Russian.
+*/
