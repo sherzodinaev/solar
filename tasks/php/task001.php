92bd486840bf2ba04cd00a686e52acb97d0023c3
Azizbek Odinaev <aziz@localhost.localdomain>
2017-12-28 00:14:08 +0500

    drafted task001

--- tasks/php/task001.php
+++ tasks/php/task001.php
@@ -15,3 +15,34 @@ and create a function that produces below output
 echo get_task001($age, $name, $languages);
 > My name is John, I am 33 years old, and I speak following languages: English, Uzbek, Russian.
 */
+<?php
+
+$age = 33;
+$name = "John";
+$languages = array('English','Uzbek','Russian');
+
+class foo 
+{
+	var $age;
+	var $name;
+	var $languages; 
+
+
+	function__constructor()
+	{
+		$this->age = '33';
+		$this->name = 'John'
+		$this->languages = array ('English','Russian','Uzbek'); 
+	}
+
+}
+
+echo<<'EOT' 
+My name is $name, I am $age years old, and I speak following languages : $languages 
+EOT;
+
+echo index.php($age, $name, $languages);
+
+?>
+
+>
