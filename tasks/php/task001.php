85c15fd2d706bdf4ea10fbfa07fd0dcffa7414e0
Sherzod Odinaev <sherzod.odinaev@accelered.com>
2017-12-28 15:02:32 -0500

    corrected task001

--- tasks/php/task001.php
+++ tasks/php/task001.php
@@ -11,38 +11,6 @@ Complete the function below
 function get_task001($age, $name, $languages)
 {
 	$result = '';
-	//your code here
+	$result = "My name is $name, I am $age years old, and I speak following languages: " . join(', ', $languages) . '.';
 	return $result;
 }
-
-<<<<<<< HEAD
-
-and create a function that produces below output
-
-echo get_task001($age, $name, $languages);
-> My name is John, I am 33 years old, and I speak following languages: English, Uzbek, Russian.
-*/
-$age = 33;
-$name = "John"
-$languages = array ('English','Uzbek','Russian');
-
-function foo {
-	echo "My name is $name, I am $age years old, and I speak following languages: $languages";
-}
->
-=======
-/*
-When tested with variables below, it should return the following output
-
-<?php
-$age = 33;
-$name = "John";
-$languages = array('English','Uzbek','Russian');
-
-echo get_task001($age, $name, $languages);
-?>
-
-> My name is John, I am 33 years old, and I speak following languages: English, Uzbek, Russian.
-
-*/
->>>>>>> 95155e46595b63fc781367c61b6683bd574b2127
